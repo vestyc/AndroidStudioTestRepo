6e418d8aca25eb0719b039e3ebf43e460fd66c64
Johnny <tranjohnny64@gmail.com>
2015-08-06 17:35:08 -0500

    update (other test button doesn't work..)

--- app/src/main/java/com/example/johnny/myapplication/MainInterface.java
+++ app/src/main/java/com/example/johnny/myapplication/MainInterface.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.RequestParams;
@@ -23,6 +24,9 @@ public class MainInterface extends AppCompatActivity implements OnJSONResponse{
     JSONArray jsonArray;
     String receptionString;
 
+    ListAdapter flyer_names;
+    ListView lv;
+
     AsyncHttpClient client = new AsyncHttpClient();
     RequestParams params = new RequestParams();
 
@@ -31,25 +35,6 @@ public class MainInterface extends AppCompatActivity implements OnJSONResponse{
     private final String getDataURL = "http://10.0.2.2:80/GitSQL/getdata.php";
 
     String names[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
-
-    public void onJSONResponse(JSONArray response) {
-
-        JSONObject obj1, obj2, obj3;
-        try {
-            obj1 = response.getJSONObject(0);
-            names[0] = obj1.getString("name");
-            obj2 = response.getJSONObject(1);
-            names[1] = obj2.getString("name");
-            obj3 = response.getJSONObject(2);
-            names[2] = obj3.getString("name");
-        }catch(JSONException e) {
-            Log.e("JSON err", "getJSONObject error", e);
-        }
-
-        ListAdapter flyer_names = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, this.names);
-        ListView lv = (ListView) findViewById(R.id.listView);
-        lv.setAdapter(flyer_names);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -86,5 +71,25 @@ public class MainInterface extends AppCompatActivity implements OnJSONResponse{
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onJSONResponse(JSONArray response) {
+
+        JSONObject obj1, obj2, obj3;
+        try {
+            obj1 = response.getJSONObject(0);
+            names[0] = obj1.getString("name");
+            obj2 = response.getJSONObject(1);
+            names[1] = obj2.getString("name");
+            obj3 = response.getJSONObject(2);
+            names[2] = obj3.getString("name");
+
+            Toast.makeText(this, names[2], Toast.LENGTH_LONG).show();
+        }catch(JSONException e) {
+            Log.e("JSON err", "getJSONObject error", e);
+        }
+
+        //flyer_names = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, this.names);
+        //lv.setAdapter(flyer_names);
     }
 }
